b314b1486e5f432fdfaf9d343f6e49c405c57392
ali <ali.nehrani@gmail.com>
2026-06-15 22:54:02 +0900

    refactor: remove greeks module and associated UI analysis section

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 // Module declarations
 pub mod api_client;
 pub mod filters;
-pub mod greeks;
 pub mod handlers;
 pub mod types;
 pub mod ui;
@@ -9,7 +8,6 @@ pub mod ui;
 // Re-exports for convenience
 pub use api_client::DeribitClient;
 pub use filters::{filter_by_creation_date, print_sample_instruments};
-pub use greeks::{calculate_collective_greeks, calculate_price_projection, CollectiveGreeks};
 pub use handlers::get_options;
 pub use types::{OptionsRequest, OptionsRequestOld};
 pub use ui::index;
